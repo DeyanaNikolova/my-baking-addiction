3e39d9744c5927bebdcaa88531855ef1cac630fb
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-12-01 16:27:26 +0000

    Generate login component

---
+++ src/app/components/user/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, Validators, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(15)]),
+  });
+
+  submit(){
+  
+  }
+}
