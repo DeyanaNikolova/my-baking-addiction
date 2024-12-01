b97582169c7481b2ebec1ec294fcd8e294343d7b
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-12-01 15:22:33 +0000

    Add routes to recipe users login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,28 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { UserComponent } from './components/user/user/user.component';
+import { UserProfileComponent } from './components/user/user-profile/user-profile.component';
+import { LoginComponent } from './components/user/login/login.component';
 
 export const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent
+    },{
+        path: 'recipes',
+        component: RecipeComponent
+    },
+    {
+        path: 'users',
+        component: UserComponent
+    },
+    {
+        path: 'user-profile', 
+        component: UserProfileComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
     }
 ];
