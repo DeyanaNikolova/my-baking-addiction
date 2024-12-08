ba968f665ecad7194c958d10a69c1780ab30d34c
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-12-08 13:17:03 +0000

    Add user details

--- src/app/components/user/user-profile/user-profile.component.ts
+++ src/app/components/user/user-profile/user-profile.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../services/user.service';
+import { AuthService } from '../../../services/auth.service';
+import { User } from '../../../models/user.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit{
 
+ user: User | undefined;
+  constructor(private userService: UserService, private authService: AuthService){}
+
+  ngOnInit(){
+const user = this.authService.getUser();
+this.user = user;
+  }
+  
 }
