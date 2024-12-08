25f4b05e5a86a13af78e19d776ed3d53d4fd66d6
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-12-08 18:33:47 +0000

    Update navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,39 +2,43 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
-
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  user: User | undefined;
 
- 
-  constructor(private router: Router, private userService: UserService) {}
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private authService: AuthService
+  ) {}
 
-  login(){
+  login() {
     this.router.navigate(['/login']);
   }
 
-  register(){
-    this.router.navigate(['/register'])
+  register() {
+    this.router.navigate(['/register']);
   }
 
-  logout(): void{
-    this.userService.logout()
+  logout(): void {
+    this.userService.logout();
     this.router.navigate(['/home']);
   }
 
-  userProfile(){
+  userProfile() {
     this.router.navigate(['/user-profile']);
   }
 
-   get isAuthenticated(): boolean{
+  get isAuthenticated(): boolean {
     return this.userService.isLogged;
   }
-
 }
