e50247673937f16968d8bb518cc73d38710e5747
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-12-08 18:36:13 +0000

    Add method getAllRecipes

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../../services/recipe.service';
+import { Recipe } from '../../models/recipe.model';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-recipe',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.css'
 })
-export class RecipeComponent {
+export class RecipeComponent implements OnInit {
 
+  recipes: Recipe[] = [];
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {
+    this.getAllRecipes();
+  }
+
+  getAllRecipes(){
+    this.recipeService.getAllRecipes().subscribe((recipes)=>{
+      this.recipes = recipes;
+    });
+  }
 }
