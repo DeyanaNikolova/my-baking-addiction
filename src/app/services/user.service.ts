b489aa64580991f5dc4faaa628bba6e6e8ef8a3c
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-12-08 10:04:01 +0000

    Add metods login register logout

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,19 +1,90 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { BehaviorSubject, Observable, Subscription, tap } from 'rxjs';
 import { environment } from '../../environments/environment.development';
 import { User } from '../models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  apiUrl = environment.api_url;
 
-apiUrl = environment.api_url;
+  private user$$ = new BehaviorSubject<User | undefined>(undefined);
+  user$ = this.user$$.asObservable();
+  user: User | undefined;
 
-  constructor(private http: HttpClient) { }
+  subscription: Subscription;
+  http = inject(HttpClient);
 
-  login(email: string, password: string): Observable<any>{
-     return this.http.post<User>(this.apiUrl + '/users/login', {email, password});
+  constructor() {
+    const token = sessionStorage.getItem('token');
+    this.subscription = this.user$.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
+  login(email: string, password: string): Observable<User> {
+    return this.http
+      .post<User>(this.apiUrl + '/users/login', { email, password })
+      .pipe(
+        tap((user) => {
+          sessionStorage.setItem('token', user.accessToken);
+          this.user$$.next(user);
+        })
+      );
+  }
+
+  register(
+    email: string,
+    username: string,
+    password: string,
+  ): Observable<User> {
+    return this.http
+      .post<User>(this.apiUrl + '/users/register', {
+        email,
+        username,
+        password,
+      })
+      .pipe(
+        tap((user) => {
+          sessionStorage.setItem('token', user.accessToken);
+          this.user$$.next(user);
+        })
+      );
+  }
+
+  getToken() {
+    return sessionStorage.getItem('token');
+  }
+
+  authHeaders() {
+    const token = this.getToken();
+    const headers = new HttpHeaders({ 'X-Authorization': token as string });
+    const options = {
+      headers: headers,
+    };
+    return options;
+  }
+
+  logout(): Observable<User> {
+   return this.http.get<User>(this.apiUrl + '/users/logout', this.authHeaders())
+    .pipe(
+      tap(() => {
+        sessionStorage.removeItem('token');
+        this.user$$.next(undefined);
+        this.isLogged;
+      }))
+    
+    
+    
+    // sessionStorage.removeItem('token');
+    // this.user = undefined;
+    // this.isLogged;
+    // this.authService.removeUser();
   }
 }
