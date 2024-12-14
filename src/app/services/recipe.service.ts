92be44cb7699ca0c6ab999c0d6c33cc2f4dacf7e
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-12-14 22:23:51 +0000

    Update recipe service

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -4,41 +4,38 @@ import { environment } from '../../environments/environment.development';
 import { Recipe } from '../models/recipe.model';
 import { Observable } from 'rxjs';
 import { UserService } from './user.service';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
-
   recipe_url = environment.recipe_url;
 
   http = inject(HttpClient);
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private authService: AuthService) {}
 
-  getAllRecipes(): Observable<Recipe[]>{
-    return this.http.get<Recipe[]>(this.recipe_url + '/recipes');
+  getRecipeDetails(id: string): Observable<Recipe> {
+    return this.http.get<Recipe>(`${this.recipe_url}/recipe/${id}`);
   }
 
-  getRecipeDetails(id: string): Observable<Recipe>{
-    return this.http.get<Recipe>(`${this.recipe_url}/recipes/${id}`);
-  }
-
-  addRecipe( recipe: Recipe): Observable<Recipe>{
-   
-    // const recipe = {
-    //   title,
-    //   shortDescription,
-    //   imageUrl,
-    //   ingredients,
-    //   instructions,
-    //   prepTime,
-    //   cookTime,
-    //   servings
-    // };
-
+  addRecipe(recipe: Recipe): Observable<Recipe> {
     const options = this.userService.authHeaders();
+    return this.http.post<Recipe>(this.recipe_url + '/recipe', recipe, options);
+  }
 
-    return this.http.post<Recipe>(this.recipe_url + '/recipes', recipe, options );
+  updateRecipe(recipe: Recipe, recipeId: string): Observable<Recipe> {
+    const options = this.userService.authHeaders();
+    return this.http.put<Recipe>(`${this.recipe_url}/recipe/${recipeId}`, recipe, options);
+  }
+
+  deleteRecipe(recipeId: string): Observable<any>{
+    const options = this.userService.authHeaders();
+    return this.http.delete(`${this.recipe_url}/recipe/${recipeId}`, options);
+  }
+
+  getAllRecipes(): Observable<Recipe[]> {
+    return this.http.get<Recipe[]>(`${this.recipe_url}/recipe`);
   }
 }
