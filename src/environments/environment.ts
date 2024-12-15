a2e9f2f37c3ae074a5d01a1bc6754b57d65f7684
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-12-15 09:24:49 +0000

    Add post url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+    api_url: 'http://localhost:3030/users',
+    recipe_url: 'http://localhost:3030/data/recipe',
+    post_url: 'http://localhost:3030/data/post',
+};
+
