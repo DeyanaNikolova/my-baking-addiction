a5a4e8c7f5c753dafee96df14f5e5c959cc65eb3
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-12-15 14:52:04 +0000

    Add date pipe

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -1,22 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../../services/recipe.service';
 import { Recipe } from '../../models/recipe.model';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MyDatePipe } from '../../pipes/date.pipe';
 
 @Component({
   selector: 'app-recipe',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [RouterModule, MyDatePipe],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.css'
 })
 export class RecipeComponent implements OnInit {
 
   recipes: Recipe[] = [];
+
   constructor(private recipeService: RecipeService) {}
-
-
+ 
   ngOnInit(): void {
     this.getAllRecipes();
   }
