1ce0fedb9047eecc72376f5fb5c100fab5d99da1
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-12-15 19:35:58 +0000

    Update recipe component

--- src/app/components/recipe/update-recipe/update-recipe.component.ts
+++ src/app/components/recipe/update-recipe/update-recipe.component.ts
@@ -7,7 +7,7 @@ import {
   FormControl,
 } from '@angular/forms';
 
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
