328303224b1b353cc23b2efecb4ebbb1bd06519f
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-12-19 18:07:41 +0000

    Change post url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     api_url: 'http://localhost:3030/users',
     recipe_url: 'http://localhost:3030/data/recipe',
-    post_url: 'http://localhost:3030/data/post',
+    comment_url: 'http://localhost:3030/data/comments',
 };
