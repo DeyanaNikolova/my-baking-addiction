369d82073b78a3c2340d85549de1c189ca5f81e8
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2025-01-12 10:24:14 +0000

    Add validations

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, Validators, FormControl, FormsModule } from '@angular/forms';
 import { UserService } from '../../../services/user.service';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   
+  emailCtrl = new FormControl('', [Validators.required, Validators.email]);
+  passwordCtrl = new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(15)]);
+ 
   loginForm: FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(15)]),
+    email: this.emailCtrl,
+    password: this.passwordCtrl,
   });
 
   constructor(private userService: UserService, private router: Router, private authService: AuthService) {}
