bd6617d423c9ffa342f6220cca477cbd9f43e584
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2025-01-12 12:17:19 +0000

    Add spinner and common module

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -3,18 +3,21 @@ import { RecipeService } from '../../services/recipe.service';
 import { Recipe } from '../../models/recipe.model';
 import { RouterModule } from '@angular/router';
 import { MyDatePipe } from '../../pipes/date.pipe';
+import { SpinnerComponent } from '../spinner/spinner.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-recipe',
   standalone: true,
-  imports: [RouterModule, MyDatePipe],
+  imports: [RouterModule, MyDatePipe, SpinnerComponent, CommonModule],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.css'
 })
 export class RecipeComponent implements OnInit {
 
   recipes: Recipe[] = [];
+  isLoading: boolean = true;
 
   constructor(private recipeService: RecipeService) {}
  
@@ -23,8 +26,15 @@ export class RecipeComponent implements OnInit {
   }
 
   getAllRecipes(){
-    this.recipeService.getAllRecipes().subscribe((recipes)=>{
-      this.recipes = recipes;
+    this.recipeService.getAllRecipes().subscribe({
+      next:recipes=>{
+        this.recipes = recipes;
+        this.isLoading = false;
+      },
+      error: (err)=>{
+        this.isLoading = false;
+        console.error(`Error: ${err}`);    
+      }  
     });
   }
 }
