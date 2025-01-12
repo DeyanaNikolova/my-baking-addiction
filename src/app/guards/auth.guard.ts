3a51eb111b6cba659c7686f5d4c1305fba2cda37
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2025-01-12 10:25:54 +0000

    Add auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,14 +1,17 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = () => {
 
   const userService = inject(UserService);
-  const router = inject(Router)
+  const router = inject(Router);
+  const authService = inject(AuthService);
   if(userService.isLogged){
     return true;
   }else{
+    authService.removeAuth();
     return router.navigateByUrl('/login');
   }
 };
