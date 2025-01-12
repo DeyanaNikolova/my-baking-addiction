c4090cd985434b0b5ba961d4062b1790e2505082
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2025-01-12 12:13:16 +0000

    Update home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { BootstrapOptions, Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Recipe } from '../../models/recipe.model';
+import { RecipeService } from '../../services/recipe.service';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  recentRecipes: Recipe[] = [];
 
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {
+    this.getRecent();
+  }
+
+  getRecent() {
+    this.recipeService.getRecent().subscribe((recent) => {
+      this.recentRecipes = recent;
+    });
+  }
 }
