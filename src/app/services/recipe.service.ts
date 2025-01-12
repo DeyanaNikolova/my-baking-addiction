8c1359e5624f70adb9f2f77ec600ec31888cdd36
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2025-01-12 10:27:45 +0000

    Refacture recipe service

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -5,7 +5,6 @@ import { Recipe } from '../models/recipe.model';
 import { Observable } from 'rxjs';
 import { UserService } from './user.service';
 
-
 @Injectable({
   providedIn: 'root',
 })
@@ -27,10 +26,14 @@ export class RecipeService {
 
   updateRecipe(recipe: Recipe, recipeId: string): Observable<Recipe> {
     const options = this.userService.authHeaders();
-    return this.http.put<Recipe>(`${this.recipe_url}/${recipeId}`, recipe, options);
+    return this.http.put<Recipe>(
+      `${this.recipe_url}/${recipeId}`,
+      recipe,
+      options
+    );
   }
 
-  deleteRecipe(recipeId: string): Observable<any>{
+  deleteRecipe(recipeId: string): Observable<any> {
     const options = this.userService.authHeaders();
     return this.http.delete(`${this.recipe_url}/${recipeId}`, options);
   }
@@ -38,8 +41,18 @@ export class RecipeService {
   getAllRecipes(): Observable<Recipe[]> {
     return this.http.get<Recipe[]>(this.recipe_url);
   }
-  
-  getRecipeById(recipeId: string): Observable<Recipe>{
+
+  getRecipeById(recipeId: string): Observable<Recipe> {
     return this.http.get<Recipe>(`${this.recipe_url}/${recipeId}`);
+  }
+
+  getRecipesByOwner(ownerId: string): Observable<Recipe> {
+    const query = encodeURIComponent(`ownerId="${ownerId}"`);
+    return this.http.get<Recipe>(`${this.recipe_url}?distinct=${query}`);
+  }
+
+  getRecent(): Observable<Recipe[]> {
+    const query = encodeURIComponent('"_createdOn"');
+    return this.http.get<Recipe[]>(`${this.recipe_url}?sortRy=${query}`);
   }
 }
